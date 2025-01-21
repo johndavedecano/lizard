c3e4a2eb8468cf3f95070042a2a04a07e7b84a61
DESKTOP-RP1HPLC\decano <johndavedecano@gmail.com>
2025-01-21 04:06:21 +0100

    added sample middleware implementation

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
 import Lizard from "./lizard";
+import type { Middleware } from "./types";
 
 const app = Lizard.create();
+
+// Global middleware to log requests
+const loggerMiddleware: Middleware = async (event, next) => {
+    console.log(`Request: ${event.method} ${event.url}`);
+    return next();
+};
+
+// Global middleware to add a custom header
+const headerMiddleware: Middleware = async (event, next) => {
+    const response = await next();
+    response.headers.set('X-Custom-Header', 'Lizard');
+    return response;
+};
+
+// Apply global middlewares
+app.use(loggerMiddleware);
+app.use(headerMiddleware);
 
 app.get('/', async (event) => {
 	return new Response("Home Page", { status: 200 });
